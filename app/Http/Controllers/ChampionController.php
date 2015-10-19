0618ff54b777a82c37ff9631c0548aad0348e36c
Ricardo Marques <ricardomarques17@gmail.com>
2015-10-19 01:36:41 +0100

    Cleans up ChampionController and makes getAllChampions call use the ApiController

--- app/Http/Controllers/ChampionController.php
+++ app/Http/Controllers/ChampionController.php
@@ -1,34 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
+//cmo é que tu em java wtv consegues fazer este tipo de acessos? :o não é só dar extend e ficas logo com os metodos do pai? o.O  sim mas acho que com o extends tens qu redefiniri ometodo mas pera
 
-use App\Http\Controllers\iApiController;
-use Illuminate\Routing\Controller as BaseController; //dunno da fuck this does xD cria um alias do routing\controller para ser usado no codigo como basecontroller
+class ChampionController extends ApiController {
 
-class ChampionController extends BaseController
-{
-    // public function getApiVersion(){
-    //     //DaFuck does he want?
-    // }
-    public function getAllChampions(){
+    public function getAllChampions() {
+        $url = $this->builURL("static-data","euw","champion","champData=image");
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?champData=image&api_key=".config('app.riotkey'));
-        //curl_setopt($ch, CURLOPT_URL, "http://www.aeiou.pt");
+        curl_setopt($ch, CURLOPT_URL,$url);
+
         curl_setopt($ch, CURLOPT_VERBOSE, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // NOT SECURE  nao faz verificação de certificados e cenas XD not important for now XD
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         // $output contains the output string 
-        $output = json_decode(curl_exec($ch),true); 
-       // die(var_dump($output['champions']));
+        $output = json_decode(curl_exec($ch), true);
+
         $title = 'Champions';
-        /*return View::make('champions')
-        ->with(array('title'=>$title,
-            'champions'=>$output));*/ 
-return view('champions',['title'=>$title,
-            'champions'=>$output['data']]);
+
+        return view('champions', ['title' => $title,
+            'champions' => $output['data']]);
     }
-
-
-
 }
