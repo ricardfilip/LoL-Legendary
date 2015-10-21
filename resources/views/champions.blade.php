ff28bf2f89f7073be74bed7fdd818876c45730fc
Ricardo Marques <ricardomarques17@gmail.com>
2015-10-21 01:46:05 +0100

    Wraps Champion list inside Bootstrap a grid

--- resources/views/champions.blade.php
+++ resources/views/champions.blade.php
@@ -1,16 +1,20 @@
 @extends('master')
 
 @section('content')
+    <div class="row text-center"><h3>Select your champion</h3></div>
+    <div class="row">
+        <div class="col-sm-12" style="overflow: hidden;">
+            <ul class="champion-list">
+                @foreach ($champions as $champion)
+                    <li class="champion-portrait">
+                        <a href="/champion/{{$champion['id']}}">
+                            <img src="http://ddragon.leagueoflegends.com/cdn/5.20.1/img/champion/{{$champion['image']['full'] }}" class="fade in">
 
-<ul class="champion-list">
-	@foreach ($champions as $champion)
-      <li class="champion-portrait">
-          <a href="/champion/{{$champion['id']}}">
-          <img src="http://ddragon.leagueoflegends.com/cdn/5.20.1/img/champion/{{$champion['image']['full'] }}">
-          <p class="text-center">{{$champion['name']}}</p>
-          </a>
-      </li>
-    @endforeach
-</ul>
-
+                            <p class="text-center">{{$champion['name']}}</p>
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
 @endsection
