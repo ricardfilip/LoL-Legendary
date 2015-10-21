224b2262de58c569d14775d977983276560bcb23
Ricardo Marques <ricardomarques17@gmail.com>
2015-10-21 01:40:27 +0100

    Links top logo to root (/) page

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -13,7 +13,7 @@
 	<nav class="navbar navbar-default">
 		<div class="container-fluid">
 			<ul class="nav navbar-nav navbar-left">
-				<li><a class="navbar-brand" href="#">LoL-Legendary</a></li>
+				<li><a class="navbar-brand" href="/">LoL-Legendary</a></li>
 			</ul>
 			<ul class="nav navbar-nav navbar-right">
 				<li><a href="#" >Login</a>
