fe0f3c1ff184bcf1f890d2a4fe3c76f14dd81074
Ricardo Marques <ricardomarques17@gmail.com>
2015-10-21 01:42:21 +0100

    Tweaks Base Stat values math to include formula from Wiki

--- public/js/main.js
+++ public/js/main.js
@@ -1,46 +1,58 @@
-/*Awesome Js Goes here*/
+//Define variables on the global scope for now
+var adResult = 0;
+var armorResult = 0;
+var mrResult = 0;
+var asResult = 0;
+var adBase = 0;
+var armorBase = 0;
+var mrBase = 0;
+var asBase = 0;
 
-function UpdateStatsPerLevel(event)
-{	
-	//Get the level selected on the dropdown list
-	//Get the table stats elements
-	//Do math
-	//Reset the results
+$(document).ready(function () {
+    //Populate Base variables on document load
+    adBase = parseFloat($("#ad .base").text());
+    armorBase = parseFloat($("#armor .base").text());
+    mrBase = parseFloat($("#mr .base").text());
+    asBase = parseFloat($("#as .base").text());
+});
 
-	var adResult = 0;
-	var armorResult = 0;
-	var mrResult = 0;
-	var asResult = 0;
+function UpdateStatsPerLevel() {
+    //Get the level selected on the dropdown list
+    //Get the table stats elements
+    var lvlSelected = $("#dropdown-lvl option:selected").val();
 
-	var lvlSelected = $( "select#dropdown-lvl option:selected").val() -1;
+    //AD CALCULATIONS
+    var adPerLevel = $("#ad .perlevel").text();
+    adResult = calculateGrowthStat(adBase,adPerLevel,lvlSelected);
+    $("#ad .base").text(Math.round(parseFloat(adResult)));
 
-	//BASE FORMULA : base stat+(statperlevel*levels)
-	//AD CALCULATIONS 
-	var adBase = $("#ad .base").text();
-	$("#ad .base").css("display","none");
-	var adPerLevel = $("#ad .perlevel").text(); 
-	adResult = parseFloat(adBase) + (parseFloat(adPerLevel) * parseFloat(lvlSelected));
-	$("#ad .result").html(Math.round(parseFloat(adResult))); 
+    //armor CALCULATIONS
+    var armorPerLevel = $("#armor .perlevel").text();
+    armorResult = calculateGrowthStat(armorBase,armorPerLevel,lvlSelected);
+    $("#armor .base").text(Math.round(parseFloat(armorResult)));
 
-	//armor CALCULATIONS 
-	var armorBase = $("#armor .base").text();
-	$("#armor .base").css("display","none");
-	var armorPerLevel = $("#armor .perlevel").text();
-	armorResult = parseFloat(armorBase) + (parseFloat(armorPerLevel) * parseFloat(lvlSelected)); //might be because of float hmm
-	$("#armor .result").html(Math.round(parseFloat(armorResult)));
+    //magic resistance CALCULATIONS
+    var mrPerLevel = $("#mr .perlevel").text();
+    mrResult = calculateGrowthStat(mrBase,mrPerLevel,lvlSelected);
+    $("#mr .base").text(Math.round(parseFloat(mrResult)));
 
-	//magic resistance CALCULATIONS 
-	var mrBase = $("#mr .base").text();
-	$("#mr .base").css("display","none");
-	var mrPerLevel = $("#mr .perlevel").text();
-	mrResult = parseFloat(mrBase) + (parseFloat(mrPerLevel) * parseFloat(lvlSelected)); //might be because of float hmm
-	$("#mr .result").html(Math.round(parseFloat(mrResult)));
-
-	//attack speed CALCULATIONS 
-	var asBase = $("#as .base").text();
-	$("#as .base").css("display","none");
-	var asPerLevel = $("#as .perlevel").text();
-	asResult = parseFloat(asBase) + (parseFloat(asPerLevel) * parseFloat(lvlSelected)); //might be because of float hmm
-	$("#as .result").html(Math.round(parseFloat(asResult)));
+    //attack speed CALCULATIONS
+    var asPerLevel = $("#as .perlevel").text();
+    asResult = calculateGrowthStat(getBaseAs(asBase),asPerLevel,lvlSelected);
+    $("#as .base").text(Math.round(parseFloat(asResult)));
 }
 
+
+function calculateGrowthStat(baseStat, perLevelStat, level) {
+    //Convert from text to numbers in case it hasn't been done before
+    baseStat = parseFloat(baseStat);
+    perLevelStat = parseFloat(perLevelStat);
+    level = parseInt(level);
+
+    //return the result
+    return baseStat + perLevelStat * ((7 / 400) * (Math.pow(level, 2) - 1) + (267 / 400) * (level - 1));
+}
+
+function getBaseAs(asOffset){
+    return (0.625/(1+asOffset));
+}
