3ebfefe0e2b60acde62b890dc0ee55cc7d4b468f
Ricardo Marques <ricardomarques17@gmail.com>
2015-10-24 02:06:18 +0100

    Creates image Cache layer so that it decides weather to serve cached or new images.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,29 +1,37 @@
 <?php
 
-/*
-  |--------------------------------------------------------------------------
-  | Application Routes
-  |--------------------------------------------------------------------------
-  |
-  | Here is where you can register all of the routes for an application.
-  | It's a breeze. Simply tell Laravel the URIs it should respond to
-  | and give it the controller to call when that URI is requested.
-  |
- */
+    /*
+      |--------------------------------------------------------------------------
+      | Application Routes
+      |--------------------------------------------------------------------------
+      |
+      | Here is where you can register all of the routes for an application.
+      | It's a breeze. Simply tell Laravel the URIs it should respond to
+      | and give it the controller to call when that URI is requested.
+      |
+     */
 
-Route::get('/', function () {
-   
-   $title = 'Legendary - Home';
-   return View::make('index')
-          ->with('title',$title);
-});
+    Route::get(
+        '/', function () {
 
-Route::get('register',function(){
-  $title = "Legendary - Register";
-  return View::make('register')
-          ->with('title',$title); //esta cena dá aqui :/
-});
+        $title = 'Legendary - Home';
+        return View::make('index')
+            ->with('title', $title);
+    }
+    );
 
-Route::get('/allchamp','ChampionController@getAllChampions');
+    Route::get(
+        'register', function () {
+        $title = "Legendary - Register";
+        return View::make('register')
+            ->with('title', $title); //esta cena dá aqui :/
+    }
+    );
 
-Route::get('/champion/{id}','ChampionController@getChampionByID');
+    Route::get('/allchamp', 'ChampionController@getAllChampions');
+
+    Route::get('/champion/{id}', 'ChampionController@getChampionByID');
+
+    //Test Image cache layer
+    Route::get("/image/{category}/{imageFile}","ImageController@getImage");
+
