f269e391f78aadc5ad16b083c741d3c1fcebdcf9
Ricardo Marques <ricardomarques17@gmail.com>
2015-10-24 02:20:08 +0100

    Starts boilerplate code on items controller

---
+++ app/Http/Controllers/ItemsController.php
@@ -0,0 +1,9 @@
+<?php
+
+    namespace App\Http\Controllers;
+
+
+    class ItemsController extends ApiController {
+        // TODO:Get all the items
+        // TODO: Find a way to generate and store tag information from all items and save (JSON) that for filtering via JS
+    }
