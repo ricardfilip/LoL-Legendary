c5f6d8ef2bbdcb7612c5729803b3c1b671b09817
Ricardo Marques <ricardomarques17@gmail.com>
2015-10-31 23:47:04 +0000

    Fixes Indentation on ChampionController

--- app/Http/Controllers/ChampionController.php
+++ app/Http/Controllers/ChampionController.php
@@ -1,30 +1,31 @@
 <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
+    class ChampionController extends ApiController {
 
-class ChampionController extends ApiController {
+        public function getAllChampions() {
+            $output = "";
+            $url = $this->buildURL("static-data", "euw", "champion", "champData=image");
+            $output = $this->apiCall($url); //Call the API
+            $output = json_decode($output, true); //Return an array
+            $output = $output['data']; //Dig into the 'data' element and discard the rest
+            ksort($output);  //Sort by Key (Champion name) for prettiness
 
-    public function getAllChampions() {
-        $output = "";
-        $url = $this->builURL("static-data", "euw", "champion", "champData=image");
-        $output = $this->apiCall($url); //Call the API
-        $output = json_decode($output, true); //Return an array
-        $output = $output['data']; //Dig into the 'data' element and discard the rest
-        ksort($output);  //Sort by Key (Champion name) for prettiness
+            $title = 'Select a Champion';
+            return view('champions', ['title' => $title, 'champions' => ($output)]);
+        }
 
-        $title = 'Select a Champion';
-        return view('champions', ['title' => $title, 'champions' => ($output)]);
+        public function getChampionByID($championId) {
+            $url = "";
+            $url = $this->buildURL("static-data", "euw", "champion/" . $championId, "champData=all"); //TODO: Decently handle Query strings
+            $champion = $this->apiCall($url);
+            $champion = json_decode($champion, true);
+            return view(
+                'champion', [
+                "title"    => $champion['name'], // *_*
+                "champion" => $champion,
+            ]
+            );
+        }
     }
-
-    public function getChampionByID($championId) {
-        $url = "";
-        $url = $this->builURL("static-data", "euw", "champion/" . $championId, "champData=all"); //TODO: Decently handle Query strings
-        $champion = $this->apiCall($url);
-        $champion = json_decode($champion, true);
-        return view('champion', [
-            "title" => $champion['name'], // *_*
-            "champion" => $champion
-        ]);
-    }
-}
