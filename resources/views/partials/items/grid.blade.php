bc5f54aa3c8315af59e56ef6f8a09f940c21a32b
Ricardo Marques <ricardomarques17@gmail.com>
2015-11-01 00:38:10 +0000

    Sets item box width and Height to match that of the items (48x48)

--- resources/views/partials/items/grid.blade.php
+++ resources/views/partials/items/grid.blade.php
@@ -1,24 +1,24 @@
 <table>
     <tbody>
     <tr>
-        <td>
+        <td style="height: 48px; width: 48px; background-color: #B0BEC5; border: 1px solid black;" class="text-center">
             1
         </td>
-        <td>
+        <td style="height: 48px; width: 48px; background-color: #B0BEC5; border: 1px solid black;" class="text-center">
             2
         </td>
-        <td>
+        <td style="height: 48px; width: 48px; background-color: #B0BEC5; border: 1px solid black;" class="text-center">
             3
         </td>
     </tr>
     <tr>
-        <td>
+        <td style="height: 48px; width: 48px; background-color: #B0BEC5; border: 1px solid black;" class="text-center">
             4
         </td>
-        <td>
+        <td style="height: 48px; width: 48px; background-color: #B0BEC5; border: 1px solid black;" class="text-center">
             5
         </td>
-        <td>
+        <td style="height: 48px; width: 48px; background-color: #B0BEC5; border: 1px solid black;" class="text-center">
             6
         </td>
     </tr>
